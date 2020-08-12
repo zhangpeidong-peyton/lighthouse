c303021a3be3549ce4d9db14f27190216709547c
Kayce Basques <kayce@google.com>
2020-08-12 16:24:29 -0700

    Add link to web.dev guide

--- lighthouse-core/audits/non-composited-animations.js
+++ lighthouse-core/audits/non-composited-animations.js
@@ -19,8 +19,8 @@ const UIStrings = {
   title: 'Avoid non-composited animations',
   /** Description of a diagnostic LH audit that shows the user animations that are not composited. */
   description: 'Animations which are not composited can be janky and contribute to CLS. ' +
-    '[Learn more](https://developers.google.com/web/fundamentals/performance/rendering/stick-to-compositor-only-properties-and-manage-layer-count)',
-  /** [ICU Syntax] Label identifying the number of animated elements that are not composited. */
+    '[Learn more](https://web.dev/non-composited-animations)',
+  /** [ICU Syntax] Label identifying the number of animations that are not composited. */
   displayValue: `{itemCount, plural,
   =1 {# animated element found}
   other {# animated elements found}
